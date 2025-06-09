3cd640e8ed13102c34795736f6217a11c405a0bb
aliym20199@gmail.com <aliym20199@gmail.com>
2025-06-09 07:59:34 +0500

    ..

---
+++ SignalRMVC/Hubs/UsersHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRMVC.Hubs;
+
+public class UsersHub : Hub
+{
+    public static int TotalViews { get; set; } = 0;
+
+    public async Task NewWindowLoaded()
+    {
+        TotalViews++;
+        await Clients.All.SendAsync("updateTotalViews",TotalViews);
+    }
+}
--- SignalRMVC/Program.cs
+++ SignalRMVC/Program.cs
@@ -1,7 +1,10 @@
+using SignalRMVC.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -24,4 +27,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapHub<UsersHub>("/hubs/userCount");
 app.Run();
