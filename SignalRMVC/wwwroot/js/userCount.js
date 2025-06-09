d9b03c10163ecc09fcfd2f8406431a1034d62024
j_sulaymonov <aliym20199@gmail.com>
2025-06-09 19:41:35 +0500

    ready

---
+++ SignalRMVC/wwwroot/js/userCount.js
@@ -0,0 +1,24 @@
+﻿
+//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+//connect methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+//invoke hub methods aka send notification to hub
+
+function newWindowsOnLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fullfilled() {
+    console.log("Успешно подключено");
+    newWindowsOnLoadedOnClient();
+}
+
+function rejectfilled() {
+    console.log("не Успешно подключено");
+}
+//start connection
+connectionUserCount.start().then(fullfilled, rejectfilled);
